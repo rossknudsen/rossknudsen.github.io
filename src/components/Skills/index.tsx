9fa029c3b8f6cfa54d4d90a984e53d2b92c206e0
Ross, Knudsen <rossknudsen@gmail.com>
2023-05-07 19:52:54 +1200

    Improve the Skills list

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,26 +1,31 @@
-import { List, ListItem, ListItemText, Rating } from "@mui/material";
+import { List, ListItem, ListItemText, Rating, Typography } from "@mui/material";
 import Skills, { ISkill, SkillType } from "../../data/Skills";
 import Foreach from "../Foreach";
+import Grid from "@mui/system/Unstable_Grid";
 
 const SkillsComponent = () => (
-  <>
+  <Grid container spacing={3}>
     <Section
       title="Languages"
       skills={Skills.filter((s, i) => s.type === SkillType.Language)}
+      key={1}
     />
     <Section
       title="Frameworks"
       skills={Skills.filter((s, i) => s.type === SkillType.Framework)}
+      key={2}
     />
     <Section
       title="Technologies"
       skills={Skills.filter((s, i) => s.type === SkillType.Technology)}
+      key={3}
     />
     <Section
       title="Tools"
       skills={Skills.filter((s, i) => s.type === SkillType.Tool)}
+      key={4}
     />
-  </>
+  </Grid>
 );
 
 interface SectionProps {
@@ -28,18 +33,21 @@ interface SectionProps {
   skills: ISkill[];
 }
 
-const Section = ({ skills }: SectionProps) => (
-  <List>
-    <Foreach
-      items={skills}
-      factory={(x, i) => (
-        <ListItem key={i}>
-          <ListItemText >{x.name}</ListItemText>
-          <Rating value={x.rating} readOnly style={{ float: "right" }} />
-        </ListItem>
-      )}
-    />
-  </List>
+const Section = ({ title, skills }: SectionProps) => (
+  <Grid xs={12} sm={6} lg={4}>
+    <Typography variant="h5">{title}</Typography>
+    <List>
+      <Foreach
+        items={skills}
+        factory={(x, i) => (
+          <ListItem key={i} sx={{paddingX: 0}}>
+            <ListItemText>{x.name}</ListItemText>
+            <Rating value={x.rating} readOnly style={{ float: "right" }} />
+          </ListItem>
+        )}
+      />
+    </List>
+  </Grid>
 );
 
-export default SkillsComponent
+export default SkillsComponent;
